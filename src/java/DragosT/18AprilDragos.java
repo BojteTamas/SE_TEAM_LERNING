fc2fda529313efcc772c7d3642de5c2d8325ff18
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-04-18 22:39:47 +0300

    18AprilProgress (#142)

---
+++ src/java/DragosT/18AprilDragos.java
@@ -0,0 +1,20 @@
+public class April18Dragos {\
+
+//    Do while and while loops!
+
+//    int x = 0; do {
+//        x++;
+//    } while(false); System.out.println(x); // Outputs 1
+
+//    while(x > 10) { x--;
+//    }
+//    and this do-while loop are simmilar (Use do while when loop is used at least once and while loop when there is
+//    is a posibility to not run the loop):
+//            if(x > 10) { do {
+//        x--;
+//    } while(x > 10);
+//    }
+
+
+
+}
