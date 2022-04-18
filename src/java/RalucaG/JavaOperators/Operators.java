a7201c1f31844ef1a0684d20c9c78faab9c5655d
Raluca Gal <45516496+RalucaIon@users.noreply.github.com>
2022-04-18 14:39:24 -0500

    18Apr2022_JavaOperators_ForLoop (#141)
    Co-authored-by: Raluca Gal <ralucaion@github.com>

--- src/java/RalucaG/JavaOperators/Operators.java
+++ src/java/RalucaG/JavaOperators/Operators.java
@@ -2,7 +2,8 @@ package JavaOperators;
 
 public class Operators{
     public static void main(String[] args) {
-        int number;
+
+      int number;
         number = 5;
         if (number > 0) {
             number = number * 2;
@@ -19,7 +20,10 @@ public class Operators{
         }else {
             System.out.println("Good Evening");
         }
+
+
 // ternary operator
+
         int y = 10;
         final int x;
         if (y > 5) {
@@ -29,13 +33,15 @@ public class Operators{
             x = 3 * y ;
         }
         System.out.println("x is: " + " " + x);
+
+//18 Apr 2022
 // left-hand boolean expression was true only j was incremented
-    /*    int j=1;
+      int j=1;
         int z=1;
 
         final int w = j<10 ? j++ : z++;
         System.out.println(j + " , " + z);
-*/
+
 //left-hand boolean expression evaluates to false only z was incremented
        int j=1;
        int z=1;
@@ -44,4 +50,50 @@ public class Operators{
 
 
     }
+
+    int roomInBelly = 5;
+    public void eatCheese(int bitesOfCheese){
+        while (bitesOfCheese > 0 && roomInBelly > 0){
+            bitesOfCheese--;
+            roomInBelly--;
+        }
+        System.out.println(bitesOfCheese + " pieces of cheese left");
+
+        //infinite loops -- the do-while statement
+
+       int r = 0;
+        do {
+            r++;
+        } while (false);
+        System.out.println(r);
+
+
+
+        // the for statement - the basic for statement
+
+        //print numbers 0 to 9
+
+        for (int i =0; i< 10; i++){
+            System.out.println(i + " ");
+        }
+
+
+        int x = 0;
+        for (long y = 0, z = 4; x <5 && y < 10; x++, y++) {
+            System.out.println(y + "  Y ");
+        }
+        System.out.println(x + "  X");
+
+        java.util.List<String> values = new java.util.ArrayList<String>();
+        values.add("Lisa");
+        values.add("Kevin");
+        values.add("Roger");
+        for (String value : values){
+            System.out.println(value + " , ");
+        }
+
+    }
+
+
+
 }
