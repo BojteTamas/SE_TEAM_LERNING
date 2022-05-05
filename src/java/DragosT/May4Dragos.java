d120e0495f372ecf1feaa4e2afd97461afb319c3
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-05-05 11:33:04 +0300

    May4Dragos (#161)

---
+++ src/java/DragosT/May4Dragos.java
@@ -0,0 +1,13 @@
+public class May4Dragos {
+//    Creating and Manipulating Strings
+    //Conctenation principles
+    System.out.println(1 + 2); // 3
+    System.out.println("a" + "b"); // ab
+    System.out.println("a" + "b" + 3); // ab3
+    System.out.println(1 + 2 + "c"); // 3c
+
+//another example result is 64
+    int three = 3;
+    String four = "4";
+    System.out.println(1 + 2 + three + four);
+}
