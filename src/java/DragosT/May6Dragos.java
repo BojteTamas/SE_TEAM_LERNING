60f977652603aa1a3db099faea0fe12240defd38
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-05-06 20:58:20 +0300

    May5Dragos (#167)

---
+++ src/java/DragosT/May6Dragos.java
@@ -0,0 +1,40 @@
+public class May6Dragos {
+//Important String Methods
+String string = "animals";
+System.out.println(string.length()); // equals 7
+System.out.println(string.charAt(0)); // prints a char at index 0
+System.out.println(string.charAt(7)); // throws exception index out of bounds
+
+System.out.println(string.indexOf("al")); // index is 4
+System.out.println(string.indexOf("al", 5)); // index returned will be -1 not found because there is not "al" starting at 5
+
+System.out.println(string.substring(3)); // mals
+System.out.println(string.substring(string.indexOf('m'))); // mals
+System.out.println(string.substring(3, 4)); // m
+System.out.println(string.substring(3, 7)); // mals
+System.out.println(string.substring(3, 3)); // empty string
+System.out.println(string.substring(3, 2)); // throws exception
+System.out.println(string.substring(3, 8)); // throws exception index out of bounds
+
+System.out.println(string.toUpperCase()); // ANIMALS
+System.out.println("Abc123".toLowerCase()); // abc123
+
+System.out.println("abc".equals("ABC")); // false
+System.out.println("ABC".equals("ABC")); // true
+System.out.println("abc".equalsIgnoreCase("ABC")); // true
+
+System.out.println("abc".startsWith("a")); // true
+System.out.println("abc".startsWith("A")); // false
+System.out.println("abc".endsWith("c")); // true
+System.out.println("abc".endsWith("a")); // false
+
+System.out.println("abc".contains("b")); // true
+System.out.println("abc".contains("B")); // false
+
+System.out.println("abcabc".replace('a', 'A')); // AbcAbc String replace(char oldChar, char newChar)
+System.out.println("abcabc".replace("a", "A")); // AbcAbc String replace(CharSequence oldChar, CharSequence newChar)
+
+System.out.println("abc".trim()); // abc removes withespace before and aftr string
+System.out.println("\t a b c\n".trim()); // a b c removes withespace before and aftr string
+
+}
