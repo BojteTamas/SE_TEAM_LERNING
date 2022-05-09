381aed2b6e92d43efa469a964f7ed1be52c921c3
Carmen <98598646+chobinca@users.noreply.github.com>
2022-05-09 18:33:03 +0300

    9may22 examples on date and time (#172)
    Co-authored-by: Carmen <chobinca@github.com>

--- src/java/CarmenH/Carmen09may.java
+++ src/java/CarmenH/Carmen09may.java
@@ -2,4 +2,73 @@ package CarmenH;
 
 public class Carmen09may {
 
+    public static void main(String[] args) {
+
+        // LocalDate d = new LocalDate(); //constructor LocalDate in class java.time.LocalDate cannot be applied to given types;
+        // LocalDate.of(2015, Month.JANUARY,32); //Invalid value for DayOfMonth (valid values 1 - 28/31): 32
+
+         /*
+        LocalDate date1 = LocalDate.of(2015, Month.JANUARY, 20);
+        LocalTime time1 = LocalTime.of(6,15);
+        LocalDateTime dateTime1 = LocalDateTime.of(2015,Month.JANUARY, 20, 6, 15, 30);
+        LocalDateTime dateTime2 = LocalDateTime.of(date1,time1);
+        System.out.println(dateTime1);
+        System.out.println(dateTime2);
+
+
+        LocalTime time1 = LocalTime.of(6,15);
+        LocalTime time2 = LocalTime.of(6,15,30);
+        LocalTime time3 = LocalTime.of(6,15,30,200);
+        System.out.println(time1);
+        System.out.println(time2);
+        System.out.println(time3);
+
+        LocalDate date1 = LocalDate.of(2015, Month.JANUARY, 20);
+        LocalDate date2 = LocalDate.of(2015, 1,20);
+        System.out.println(date1);
+        System.out.println(date2);
+
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+
+ */
+/*
+        List<String> list = new ArrayList<>();
+        list.add("hawk");
+        list.add("robin");
+        Object[] objectArray = list.toArray();
+        System.out.println(objectArray.length);
+        String[] stringArray=list.toArray(new String[0]);
+        System.out.println(stringArray.length);
+
+        String[] arr =  {"hawk", "robin"};
+        System.out.println(arr); //[Ljava.lang.String;@75b84c92
+        System.out.println(arr[0] + " " + arr[1]);
+
+        List<String> listt=Arrays.asList(arr); // i don't understand here - why
+        System.out.println(list.size());
+        listt.set(1, "test");
+        arr[0]="new";
+        for(String b: arr) System.out.println(b + " ");
+        //listt.remove(1); //we are not allowed to change the size of list
+
+
+        List<String> lista = Arrays.asList("one", "two");
+        System.out.println(lista);
+
+
+
+
+        //sorting
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(99);
+        numbers.add(5);
+        numbers.add(81);
+        Collections.sort(numbers);
+        System.out.println(numbers); // [5, 81, 99]
+
+         */
+    }
+
 }
