99bc06463eb4354e4a86eefa6d7f9d1876dd476b
bws <mdunca92>
2022-05-10 21:26:52 +0300

    MariaD 10_may_2022 commit

---
+++ src/java/MariaD/10_may.java
@@ -0,0 +1,48 @@
+/*
+The while statement
+uses nonconstant variables
+each repetition of the statement may be different
+ex:
+    int roomInBelly = 5;
+    public void eatCheese (int bitesOfCheese) {
+        while (bitesOfCheese > 0 && roomInBelly > 0) {
+            bitesOfCheese--;
+            roomInBelly--;
+        }
+        System.out.println(bitesOfCheese+"pieces of cheese left");
+    }
+
+    INFINITE LOOPS
+
+    DO-WHILE STATEMENT
+    The primary difference : do-while loop purposely orders the statement or block of statements
+                                before the conditional expression - to reinforce that
+                                the statement will be executed before the expression is ever evaluated
+
+                                ex:
+                                int x = 0;
+                                do {
+                                    x++;
+                                 }
+                              while(false);
+                              System.out.println(x);
+
+
+     FOR STATEMENT
+     FOR LOOP
+     EX:
+     for (int i= 0; i < 10; i++) {
+     System.out.println(i + "");
+     }
+
+     int x = 1;
+     long y = 10;
+     for (y = 0, x=4, x<5 && y<10; x++, y++) {
+     System.out.println(x + " ");
+     }
+
+
+     for each statement
+
+
+ */
