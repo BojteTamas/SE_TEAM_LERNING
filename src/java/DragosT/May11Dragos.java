08542550566415909b5c329286201ccadb3e654f
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-05-11 23:03:14 +0300

    May11Dragos

---
+++ src/java/DragosT/May11Dragos.java
@@ -0,0 +1,14 @@
+public class May11Dragos {
+
+//StringBuilder Methods
+    //charAt(), indexOf(), length(), and substring()
+
+
+StringBuilder sb = new StringBuilder("animals");
+String sub = sb.substring(sb.indexOf("a"), sb.indexOf("al"));//from a to al -> anim
+    int len = sb.length();//7
+    char ch = sb.charAt(6);//s
+    System.out.println(sub + " " + len + " " + ch); //anim 7 s
+
+
+}
