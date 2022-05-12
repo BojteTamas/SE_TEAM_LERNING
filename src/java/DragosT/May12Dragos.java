8c2f1df4aa842b97fa298445e3f872efa05b6f59
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-05-12 21:08:56 +0300

    May12Dragos (#188)

---
+++ src/java/DragosT/May12Dragos.java
@@ -0,0 +1,10 @@
+public class May12Dragos {
+    //append()
+    StringBuilder sb = new StringBuilder().append(1).append('c'); sb.append("-").append(true);
+    System.out.println(sb); //You can chain append() without declaring string first -> 1c-true
+
+    StringBuilder insert(int offset, String str) // inserts at the index the string
+    StringBuilder sb = new StringBuilder("animals");
+    sb.insert(7, "-"); // 7 will be at the end of animals -> animals-
+
+}
