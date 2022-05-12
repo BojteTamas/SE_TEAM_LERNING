8a1c5329748fc5fb4104d900b4afe53de8f55c1e
mdunca92 <98758996+mdunca92@users.noreply.github.com>
2022-05-12 17:55:29 +0300

    mariad 12_may_2022 commit (#186)
    Co-authored-by: bws <mdunca92>

--- src/java/MariaD/Main.java
+++ src/java/MariaD/Main.java
@@ -1,9 +1,12 @@
-package MariaDunca;
+/*package MariaDunca;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // write your code here
+   public static void main(String[] args) {
+       write your code here
         System.out.println("Hello package");
-    }
+ }
 }
+
+/*
+ */
