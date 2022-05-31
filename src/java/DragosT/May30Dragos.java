9e3482290eb6f372e2915bd0e47c86d5f627075a
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-06-01 00:11:01 +0300

    May30Dragos

---
+++ src/java/DragosT/May30Dragos.java
@@ -0,0 +1,19 @@
+public class May30Dragos {
+//date time
+import java.time.*;
+System.out.println(LocalDate.now()); //y-m-h
+System.out.println(LocalTime.now()); //h:m:s.ms
+System.out.println(LocalDateTime.now());//y-m-hh:m:s.ms
+
+LocalDate date = LocalDate.of(2014, Month.JANUARY, 20);
+System.out.println(date); // 2014-01-20
+date = date.plusDays(2);
+System.out.println(date); // 2014-01-22
+date = date.plusWeeks(1);
+System.out.println(date); // 2014-01-29
+date = date.plusMonths(1);
+System.out.println(date); // 2014-02-28
+date = date.plusYears(5);
+System.out.println(date);// 2019-02-28
+// sami with minus minusYears(1) will produce 2019-02-28
+}
