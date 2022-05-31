d9f2312eb22702d92cde9fba3896633d9c38c175
Carmen <chobinca@github.com>
2022-05-31 15:56:32 +0300

    30may22 about static ch4 oca8

---
+++ src/java/CarmenH/may31/Static.java
@@ -0,0 +1,21 @@
+package CarmenH.may31;
+
+public class Static {
+
+    private static String name = "Static class"; //added static
+
+    public static void first() {
+            }
+    public static void second() {
+            }
+    public static void third() { //added static
+        System.out.println(name);
+    }
+
+    public static void main(String args[]) {
+        first();
+        second();
+       // new Static().third(); //this would solve the problem from main
+        third();
+    }
+}
