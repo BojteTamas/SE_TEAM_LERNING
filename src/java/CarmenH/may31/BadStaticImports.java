dfc906a8d205063f3802ef53ebcd201b93febac0
Carmen <98598646+chobinca@users.noreply.github.com>
2022-06-02 09:08:05 +0300

    01jun22 - passing data values vs reference (#238)
    Co-authored-by: Carmen <chobinca@github.com>

--- src/java/CarmenH/may31/BadStaticImports.java
+++ src/java/CarmenH/may31/BadStaticImports.java
@@ -1,17 +1,18 @@
 package CarmenH.may31;
-//import static java.util.Arrays;
+//import static java.util.Arrays; // tries to use a static import to import a class. Remember that static imports are only for importing static members.Regular imports are for importing a class
 import java.util.Arrays;
 import static java.util.Arrays.asList;
-//static import java.util.Arrays.*;
+//static import java.util.Arrays.*; //tries to see if you are paying attention to the order of keywords. The syntax is import static and not vice versa.
 
 
 public class BadStaticImports {
 
     public static void main(String[] args) {
         Arrays.asList("one"); // DOES NOT COMPILE //import java.util.Arrays;
+        // above Line  is sneaky. We imported the asList method on line 4.
+        // However, we did not import the Arrays class anywhere. This makes it okay to write asList("one"); but not Arrays.asList("one");
     }
 }
 
-//Line 2 tries to use a static import to import a class. Remember that static imports are only for importing static members.Regular imports are for importing a class
-//Line 4 tries to see if you are paying attention to the order of keywords. The syntax is import static and not vice versa.
-//Line 10 is sneaky. We imported the asList method on line 3. However, we did not import the Arrays class anywhere. This makes it okay to write asList("one"); but not Arrays.asList("one");.
+
+
