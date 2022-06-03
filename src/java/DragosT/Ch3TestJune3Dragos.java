45528fe99cf6bf2abb04ab80c6fe8bb80ef86f17
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-06-03 20:49:49 +0300

    13

---
+++ src/java/DragosT/Ch3TestJune3Dragos.java
@@ -0,0 +1,36 @@
+public class Ch3TestJune3Dragos {
+//1. Answr:A,D     Actual:
+//2. Answr:A,C,D,E     Actual:
+//3. Answr:B,D,E     Actual:
+//4. Answr:B     Actual:
+//5. Answr:D     Actual:
+//6. Answr:D     Actual:
+//7. Answr:E,F    Actual:
+//8. Answr:A,E     Actual:
+//9. Answr:C     Actual:
+//10. Answr:C     Actual:
+//11. Answr:E,F     Actual:
+//12. Answr:A     Actual:
+//13. Answr:B     Actual:
+//14. Answr:     Actual:
+//15. Answr:     Actual:
+//16. Answr:     Actual:
+//17. Answr:     Actual:
+//18. Answr:     Actual:
+//19. Answr:     Actual:
+//20. Answr:     Actual:
+//21. Answr:     Actual:
+//22. Answr:     Actual:
+//23. Answr:     Actual:
+//24. Answr:     Actual:
+//25. Answr:     Actual:
+//26. Answr:     Actual:
+//27. Answr:     Actual:
+//28. Answr:     Actual:
+//29. Answr:     Actual:
+//30. Answr:     Actual:
+//31. Answr:     Actual:
+//32. Answr:     Actual:
+//33. Answr:     Actual:
+
+}
