7b54131b57da5ff370d83c8676e84ee1a7e1f517
Carmen <98598646+chobinca@users.noreply.github.com>
2022-06-06 13:19:25 +0300

    06jun22 Order of Initialization (#249)
    Co-authored-by: Carmen <chobinca@github.com>

---
+++ src/java/CarmenH/june06/CallInitializationOrderSimple.java
@@ -0,0 +1,10 @@
+package CarmenH.june06;
+
+public class CallInitializationOrderSimple {
+
+    public static void main(String[] args) {
+        InitializationOrderSimple init = new InitializationOrderSimple();
+    }
+
+}
+
