7f7be4227e41a89948cc6ed0764737436456eee3
Carmen <98598646+chobinca@users.noreply.github.com>
2022-06-09 11:35:56 +0300

    09jun22 Lambda & Predicates - finished chapter 4 (#263)
    Co-authored-by: Carmen <chobinca@github.com>

---
+++ src/java/CarmenH/june09/ButtonListenerNewWay.java
@@ -0,0 +1,28 @@
+package CarmenH.june09;
+import java.awt.*;
+
+
+public class ButtonListenerNewWay {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static void main(String[] args) {
+        Frame frame=new Frame("ActionListener java8");
+
+        Button b=new Button("Click Here");
+        b.setBounds(50,100,80,50);
+
+        /**
+         * b.addActionListener(new ActionListener(){
+         *             public void actionPerformed(ActionEvent e){ //action performed = click - it prints the text
+         *                 System.out.println( ANSI_RED + "Hello World!");
+         *             }
+         *         });
+         */
+
+        b.addActionListener(e -> System.out.println( ANSI_RED +"Hello World!"));
+        frame.add(b);
+
+        frame.setSize(200,200);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
---
+++ src/java/CarmenH/june09/Carmen09jun.java
@@ -0,0 +1,14 @@
+package CarmenH.june09;
+
+public class Carmen09jun {
+
+    public static void main ( String[] args)
+    {
+       // print(() -> true);
+       // print(a -> a.startsWith("test"));
+    }
+
+
+
+
+}
