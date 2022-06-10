c3a99540d743f04a99da71d0a4d17bdf71518163
Bojte Tamas <tamas.bojte@externals.adidas.com>
2022-06-10 13:21:04 +0300

    Done for today June 10

--- src/main/java/TamasB/Test.java
+++ src/main/java/TamasB/Test.java
@@ -1,5 +1,20 @@
 package TamasB;
 
 public class Test {
-    //a
+
+  public static void main(String[] args) {
+    String unu = "";
+
+    unu = apendAllStrings("unu", "lasdfsa", "sadsadsa");
+    System.out.println(unu);
+  }
+
+  public static String apendAllStrings(String... strings) { // => String[] strings
+    String initial = "";
+    for (String string : strings) {
+      initial = initial + string;
+    }
+
+    return initial;
+  }
 }
