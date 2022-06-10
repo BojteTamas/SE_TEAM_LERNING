a86a657bfa5f5d640de6af8261980001b4d6ebae
Bojte Tamas <bojtetomi92@gmail.com>
2022-06-10 13:22:12 +0300

    Demo time (#270)
    * Demo time
    
    * Update Test.java
    
    * Done for today June 10
    
    Co-authored-by: Bojte Tamas <tamas.bojte@externals.adidas.com>

--- src/main/java/TamasB/Test.java
+++ src/main/java/TamasB/Test.java
@@ -1,3 +1,20 @@
 package TamasB;
 
-public class Test {}
+public class Test {
+
+  public static void main(String[] args) {
+    String unu = "";
+
+    unu = apendAllStrings("unu", "lasdfsa", "sadsadsa");
+    System.out.println(unu);
+  }
+
+  public static String apendAllStrings(String... strings) { // => String[] strings
+    String initial = "";
+    for (String string : strings) {
+      initial = initial + string;
+    }
+
+    return initial;
+  }
+}
