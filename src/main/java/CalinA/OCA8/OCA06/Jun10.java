27f0cf8015d9273d104dd1f4caa5c0fbdfd87c7f
CalinAndreca <101342607+CalinAndreca@users.noreply.github.com>
2022-06-10 18:32:45 +0300

    OCAJun10 (#272)
    * OCAJun10
    
    * UpdatedfilesJun10
    
    * Delete BadStaticImports.java
    
    am sters fisierul
    
    Co-authored-by: bws <bws@bwss-MacBook-Pro.local>
    Co-authored-by: Carmen <98598646+chobinca@users.noreply.github.com>

---
+++ src/main/java/CalinA/OCA8/OCA06/Jun10.java
@@ -0,0 +1,14 @@
+package CalinA.OCA8.OCA06;
+
+public class Jun10 {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 10;
+    int c = 20;
+    System.out.println("Este " + a + " plus " + b + " egal? " + (a == b));
+    System.out.println("Este " + b + " plus " + c + " egal? " + (b == c));
+  }
+}
+
+// Read from OCA
+//        Equality Operators
