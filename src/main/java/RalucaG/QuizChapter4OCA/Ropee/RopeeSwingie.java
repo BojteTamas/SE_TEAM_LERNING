5ddb8b9ab431f81e270cbd0862fd3469a8b851da
Raluca Gal <ralu2491@gmail.com>
2022-06-14 13:41:43 +0300

    June14th_QuizFinished

---
+++ src/main/java/RalucaG/QuizChapter4OCA/Ropee/RopeeSwingie.java
@@ -0,0 +1,17 @@
+package RalucaG.QuizChapter4OCA.Ropee;
+
+public class RopeeSwingie {
+  private static Ropeee ropeee1 = new Ropeee();
+  private static Ropeee ropeee2 = new Ropeee();
+
+  {
+    System.out.println(ropeee1.length);
+  }
+
+  public static void main(String[] args) {
+    ropeee1.length = 2;
+    ropeee2.length = 8;
+
+    System.out.println(ropeee1.length);
+  }
+}
---
+++ src/main/java/RalucaG/QuizChapter4OCA/StringBuilderr.java
@@ -0,0 +1,18 @@
+package RalucaG.QuizChapter4OCA;
+
+public class StringBuilderr {
+  public static StringBuilder working(StringBuilder a, StringBuilder b) {
+    a = new StringBuilder("a");
+    b.append("b");
+    return a;
+  }
+
+  public static void main(String[] args) {
+    StringBuilder s1 = new StringBuilder("s1");
+    StringBuilder s2 = new StringBuilder("s2");
+    StringBuilder s3 = working(s1, s2);
+    System.out.println("s1 = " + s1);
+    System.out.println("s2 = " + s2);
+    System.out.println("s3 = " + s3);
+  }
+}
