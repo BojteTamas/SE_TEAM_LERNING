5bde6c504a8d7e4eea95180a15e0b1442719e61b
CalinAndreca <101342607+CalinAndreca@users.noreply.github.com>
2022-06-14 07:14:13 +0300

    OCAJun13 (#279)
    Co-authored-by: bws <bws@bwss-MacBook-Pro.local>

---
+++ src/main/java/CalinA/OCA8/OCA06/Jan13.java
@@ -0,0 +1,14 @@
+package CalinA.OCA8.OCA06;
+
+public class Jan13 {
+  public static void main(String[] args) {
+    int x = 15;
+    if (x < 10) System.out.println("x is smaller than 10");
+    else System.out.println("x is greater than 10");
+    System.out.println("Outside if-else block");
+  }
+}
+
+// Read from OCA:
+//        The if-then Statement
+//        The if-then-else Statement
