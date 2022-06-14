65347080097cc8a92dede3e678cd7bcc1d6f1a1f
bws <bws@bwss-MacBook-Pro.local>
2022-06-14 16:19:27 +0300

    Jun14OCA

---
+++ src/main/java/CalinA/OCA8/OCA06/Jun14.java
@@ -0,0 +1,18 @@
+package CalinA.OCA8.OCA06;
+
+public class Jun14 {
+    public static void main(String[] args) {
+
+        int num1 = 1, num2 = 5, res;
+        System.out.println("First num: " + num1);
+        System.out.println("Secound num: " + num2);
+
+            res = ((num1 > num2) ? (num1 + num2) : (num1 - num2));
+            System.out.println("Result = " + res);
+
+
+    }
+}
+
+//Read from OCA
+//        Ternary Operator
