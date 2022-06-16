fcef2facab52b0b071b140d312b0b2ec3cdcb748
CalinAndreca <101342607+CalinAndreca@users.noreply.github.com>
2022-06-16 15:01:27 +0300

    OCAJun16 (#290)
    Co-authored-by: bws <bws@bwss-MacBook-Pro.local>

---
+++ src/main/java/CalinA/OCA8/OCA06/Jun16.java
@@ -0,0 +1,15 @@
+package CalinA.OCA8.OCA06;
+
+public class Jun16 {
+  public static void main(String[] args) {
+    int x = 10, sum = 0;
+    while (x <= 20) {
+      sum = sum + x;
+      x++;
+    }
+    System.out.println("Insumarea: " + sum);
+  }
+}
+
+// Read from OCA:
+//        The while Statement
