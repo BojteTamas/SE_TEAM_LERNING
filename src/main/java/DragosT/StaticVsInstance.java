91f83c431701280cd88e4fd04381d77ccc475c92
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-06-17 18:41:11 +0300

    StaticInstanceEx

---
+++ src/main/java/DragosT/StaticVsInstance.java
@@ -0,0 +1,18 @@
+package DragosT;
+
+public class StaticVsInstance {
+    public static int count;
+    public static void count1() { count++; }
+    public void count2() { count++; }
+    public void announceCounts() {
+        count1();
+        count2(); }
+    public static void announceCountsToEveryone() { count1();
+        //count2(); // DOES NOT COMPILE a static method is calling an instance method
+    }
+    public static int total;
+    //public static average = total / count; // DOES NOT COMPILE a static variable is
+    // calling an instance variable
+
+
+}
