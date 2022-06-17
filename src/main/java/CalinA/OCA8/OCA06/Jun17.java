9f56328a622b8f6746fc212a42eb0eded7cb3e04
bws <bws@bwss-MacBook-Pro.local>
2022-06-17 14:49:01 +0300

    OCAJun17

---
+++ src/main/java/CalinA/OCA8/OCA06/Jun17.java
@@ -0,0 +1,14 @@
+package CalinA.OCA8.OCA06;
+
+public class Jun17 {
+  public static void main(String[] args) {
+    int x = 1;
+    do {
+      System.out.println("Afara ploua!");
+      x++;
+    } while (x < 5);
+  }
+}
+
+//Read from OCA:
+//        The do-while Statement
