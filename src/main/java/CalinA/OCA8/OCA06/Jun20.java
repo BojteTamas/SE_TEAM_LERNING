f178618670a661166d5cef699ee71bd7a85794af
bws <bws@bwss-MacBook-Pro.local>
2022-06-20 14:52:53 +0300

    OCAJun20

---
+++ src/main/java/CalinA/OCA8/OCA06/Jun20.java
@@ -0,0 +1,13 @@
+package CalinA.OCA8.OCA06;
+
+public class Jun20 {
+  public static void main(String[] args) {
+    {
+      for (int i = 1; i <= 10; i++) System.out.println(i);
+    }
+  }
+}
+
+// Read form OCA
+//        The for Statement
+//        The Basic for Statement
