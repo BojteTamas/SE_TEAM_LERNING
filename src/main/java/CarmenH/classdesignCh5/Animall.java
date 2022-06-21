556a3009f14bd10265e0fd3e67e6fb601722db1f
Carmen <chobinca@github.com>
2022-06-21 12:50:19 +0300

    21jun22 ch5 day4

---
+++ src/main/java/CarmenH/classdesignCh5/Animall.java
@@ -0,0 +1,6 @@
+package CarmenH.classdesignCh5;
+
+public abstract class Animall {
+    public abstract String getName();
+
+}
---
+++ src/main/java/CarmenH/classdesignCh5/HasTail.java
@@ -0,0 +1,5 @@
+package CarmenH.classdesignCh5;
+
+public interface HasTail {
+    public int getTailLength();
+}
