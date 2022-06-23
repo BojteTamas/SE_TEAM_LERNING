8c4753d3430dbcc633628046fcf9e261f3a1a9a9
bws <bws@bwss-MacBook-Pro.local>
2022-06-23 16:05:35 +0300

    OCAJun23

---
+++ src/main/java/CalinA/OCA8/OCA06/Jun23.java
@@ -0,0 +1,14 @@
+package CalinA.OCA8.OCA06;
+
+public class Jun23 {
+  public static void main(String[] args) {
+    for (int x = 10; x < 20; x++) {
+      if (x == 16) break;
+      System.out.println("x= " + x);
+    }
+    System.out.println("Completed the Loop.");
+  }
+}
+
+// Readm from OCA
+//        The break Statement
