38361243cdd0a758d23c6619e84d630f9e98c92e
mdunca92 <98758996+mdunca92@users.noreply.github.com>
2022-06-27 21:33:37 +0300

    mariad 27_june methods (#327)
    * mariad 27_june methods
    
    * updated file
    
    Co-authored-by: bws <mdunca92>

---
+++ src/main/java/MariaD/june_27/Metoda_fara_parametru_si_fara_return.java
@@ -0,0 +1,24 @@
+package MariaD.june_27;
+
+import java.util.*;
+
+public class Metoda_fara_parametru_si_fara_return {
+  int x, y; // / intializam variabile x, y ca si constructori, care vor fi utilizati direct in cadru
+  // metodei add()
+
+  Metoda_fara_parametru_si_fara_return(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public void add() { // metoda cu paranteze () ->  metoda fara parametru si fara tip de returnare
+    int sum = x + y;
+    System.out.println("sum is" + sum);
+  }
+
+  public static void main(String[] args) {
+    Metoda_fara_parametru_si_fara_return metoda =
+        new Metoda_fara_parametru_si_fara_return(1, 5); // /sum is 6
+    metoda.add();
+  }
+}
---
+++ src/main/java/MariaD/june_27/Standard_method.java
@@ -0,0 +1,9 @@
+package MariaD.june_27;
+
+import java.util.*;
+
+public class Standard_method {
+  public static void main(String[] args) {
+    System.out.println("Aceasta e o metoda standard");
+  }
+}
