0fe576ab6ad7008c008f2ff44bd53ed07fb10778
bws <bws@bwss-MacBook-Pro.local>
2022-06-28 12:35:03 +0300

    Chapter3OCAJun28

---
+++ src/main/java/CalinA/OCA8/OCA06/Jun28.java
@@ -0,0 +1,19 @@
+package CalinA.OCA8.OCA06;
+
+public class Jun28 {
+  public static void main(String[] args) {
+    String con1 = "Good-";
+    String con2 = "-Morning";
+    String con3 = con1.concat(con2);
+
+    System.out.println(con3);
+
+    String con4 = "--World";
+    String con5 = con3.concat(con4);
+
+    System.out.println(con5);
+  }
+}
+// Started Cahpter3, read from OCA:
+//        Creating and Manipulating Strings
+//        Concatenation
