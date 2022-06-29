25b06039c623f5524ae9aa2796e01d343e6a0d35
bws <bws@bwss-MacBook-Pro.local>
2022-06-29 14:30:44 +0300

    OCAJun29

---
+++ src/main/java/CalinA/OCA8/OCA06/Jun29.java
@@ -0,0 +1,14 @@
+package CalinA.OCA8.OCA06;
+
+public class Jun29 {
+  public static void main(String[] args) {
+    String n1 = new String("12345");
+    String n2 = new String("12345");
+
+    if (n1 == n2) System.out.println("Yes");
+    else System.out.println("No");
+  }
+}
+// Read from OCA:
+//        Immutability
+//        The String Pool
