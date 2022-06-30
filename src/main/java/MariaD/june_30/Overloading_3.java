93bb979810fd782198f88a3404794e5dc03d4484
bws <mdunca92>
2022-06-30 17:03:22 +0300

    mariad daily commit overloading methods

---
+++ src/main/java/MariaD/june_30/Overloading_3.java
@@ -0,0 +1,19 @@
+package MariaD.june_30;
+
+class Overloading1 {
+  public void display(char litere, int number) {
+    System.out.println("prima metoda overloading");
+  }
+
+  public void display(int number, char c) {
+    System.out.println("a doua metoda overloading");
+  }
+}
+
+public class Overloading_3 {
+  public static void main(String[] args) {
+    Overloading1 metoda = new Overloading1();
+    metoda.display('a', 2); // prima metoda overloading
+    metoda.display(3, 'b'); // a doua metoda overloading
+  }
+}
---
+++ src/main/java/MariaD/june_30/Overloading_method.java
@@ -0,0 +1,34 @@
+package MariaD.june_30;
+
+import java.util.*;
+
+/*
+Method overloading - different method signatures with the same name but different type parameters.
+it means there can be different access modifiers, specifiers (like static), return types, and exception lists.
+ */
+
+public class Overloading_method {
+  public void fluture(int fluturas) {
+    fluturas = 6;
+    System.out.println(fluturas);
+  }
+
+  public void fluture(short aripi) {
+    aripi = 10000;
+    System.out.println(aripi);
+  }
+
+  public boolean fluture() {
+    return true;
+  }
+
+  void fluture(int fluturas, short aripi) {}
+
+  public static void main(String[] args) {
+    Overloading_method metoda = new Overloading_method();
+    metoda.fluture('a'); // 6
+    metoda.fluture(
+        'b'); // 6///de ce a printat tot 6 si nu a printat 100000, si de ce nu pot sa pun aripi, imi
+    // da tot fluture?
+  }
+}
