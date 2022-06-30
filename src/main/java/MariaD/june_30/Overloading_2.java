667cd2dd94c07893b709a4ad1c1f0d9c51421e03
mdunca92 <98758996+mdunca92@users.noreply.github.com>
2022-06-30 23:41:05 +0300

    mariad daily commit overloading methods (#337)
    Co-authored-by: bws <mdunca92>

---
+++ src/main/java/MariaD/june_30/Overloading_2.java
@@ -0,0 +1,29 @@
+package MariaD.june_30;
+
+class Overloading {
+  public void display(char c) {
+    System.out.println(c);
+  }
+
+  public void display(int c) {
+    System.out.println(c);
+  }
+
+  public void display(String c) {
+    System.out.println(c);
+  }
+
+  public boolean display(boolean c) {
+    return true;
+  }
+}
+
+public class Overloading_2 {
+  public static void main(String[] args) {
+    Overloading obiect = new Overloading();
+    obiect.display('a'); // a
+    obiect.display(1000); // 1000
+    obiect.display("vine vacanta"); // vine vacanta
+    obiect.display(true);
+  }
+}
