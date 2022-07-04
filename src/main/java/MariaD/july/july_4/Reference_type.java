e72094d266dfe1c885bb2595aaf674f1ece8e69e
bws <mdunca92>
2022-07-04 21:54:26 +0300

    mariad daily commit

---
+++ src/main/java/MariaD/july/july_4/Reference_type.java
@@ -0,0 +1,21 @@
+package MariaD.july.july_4;
+
+import java.util.*;
+
+/// se face autoboxing
+public class Reference_type {
+  public void fluture(String aripa) {
+    System.out.println("string");
+  }
+
+  public void fluture(int i) {
+    System.out.println("int type");
+  }
+
+  public static void main(String[] args) {
+    Reference_type referinta = new Reference_type();
+    referinta.fluture("test"); // string
+    referinta.fluture(1); // int type
+  }
+}
+// raspunsul corect ar trebui sa fie string, de ce se face autoboxing?
