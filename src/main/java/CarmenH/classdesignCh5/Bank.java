50149dbdcee6aff1198fb8a39834d01d9bd6a827
Carmen <98598646+chobinca@users.noreply.github.com>
2022-07-04 17:34:34 +0300

    4jul22 finished ch5, recap (#344)
    Co-authored-by: Carmen <chobinca@github.com>

---
+++ src/main/java/CarmenH/classdesignCh5/Bank.java
@@ -0,0 +1,7 @@
+package CarmenH.classdesignCh5;
+
+public class Bank {
+  float getRateOfInterest() {
+    return 0;
+  }
+}
---
+++ src/main/java/CarmenH/classdesignCh5/ICICI.java
@@ -0,0 +1,7 @@
+package CarmenH.classdesignCh5;
+
+public class ICICI extends Bank {
+  float getRateOfInterest() {
+    return 7.3f;
+  }
+}
