d000df1b7bbca65d3f2218d523521ee3ea4f7d8f
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-07-06 22:06:55 +0300

    Ch4 test dragos (#358)
    * Test4 recap and begining
    
    * Test4 recap and begining
    
    * Test4 recap and begining spotless

---
+++ src/main/java/DragosT/Ch4TestDragos.java
@@ -0,0 +1,36 @@
+package DragosT;
+
+public class Ch4TestDragos {
+  // read and recap the chapter for test 4
+
+  // 1.  Answr:B,C       Actual:
+  // 2.  Answr:A,D       Actual:
+  // 3.  Answr:A,C,D     Actual:
+  // 4.  Answr:A,B,G     Actual:
+  // 5.  Answr:     Actual:
+  // 6.  Answr:     Actual:
+  // 7.  Answr:     Actual:
+  // 8.  Answr:     Actual:
+  // 9.  Answr:     Actual:
+  // 10. Answr:     Actual:
+  // 11. Answr:     Actual:
+  // 12. Answr:     Actual:
+  // 13. Answr:     Actual:
+  // 14. Answr:     Actual:
+  // 15. Answr:     Actual:
+  // 16. Answr:     Actual:
+  // 17. Answr:     Actual:
+  // 18. Answr:     Actual:
+  // 19. Answr:     Actual:
+  // 20. Answr:     Actual:
+  // 21. Answr:     Actual:
+  // 22. Answr:     Actual:
+  // 23. Answr:     Actual:
+  // 24. Answr:     Actual:
+  // 25. Answr:     Actual:
+  // 26. Answr:     Actual:
+  // 27. Answr:     Actual:
+  // 28. Answr:     Actual:
+  // 29. Answr:     Actual:
+
+}
