6fcb4699b19db2b7457d965c5f2eb03b50fe696e
bws <bws@bwss-MacBook-Pro.local>
2022-07-06 14:56:08 +0300

    OCAJul06

---
+++ src/main/java/CalinA/OCA8/JulOCA/StringBuilderMethods/Jul06charAt.java
@@ -0,0 +1,12 @@
+package CalinA.OCA8.JulOCA.StringBuilderMethods;
+
+public class Jul06charAt {
+  public static void main(String[] args) {
+    String x = "This is charAt() method.";
+    char ch = x.charAt(5);
+    System.out.println(ch);
+
+    ch = x.charAt(0);
+    System.out.println(ch);
+  }
+}
---
+++ src/main/java/CalinA/OCA8/JulOCA/StringBuilderMethods/Jul06substring.java
@@ -0,0 +1,9 @@
+package CalinA.OCA8.JulOCA.StringBuilderMethods;
+
+public class Jul06substring {
+  public static void main(String[] args) {
+    String sub = new String("This is substring method");
+    System.out.print("The substring is:");
+    System.out.println(sub.substring(7, 17));
+  }
+}
