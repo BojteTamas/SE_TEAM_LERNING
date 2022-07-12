90057095bdc572c4c50fc87509bf09223d05a8a5
CalinAndreca <101342607+CalinAndreca@users.noreply.github.com>
2022-07-12 13:56:45 +0300

    OCAJul12 (#372)
    Co-authored-by: bws <bws@bwss-MacBook-Pro.local>

---
+++ src/main/java/CalinA/OCA8/JulOCA/Reverse/Jul12reverse.java
@@ -0,0 +1,11 @@
+package CalinA.OCA8.JulOCA.Reverse;
+
+public class Jul12reverse {
+  public static void main(String[] args) {
+    String rev = "Today is raining";
+    StringBuilder rev1 = new StringBuilder();
+    rev1.append(rev);
+    rev1.reverse();
+    System.out.println(rev1);
+  }
+}
