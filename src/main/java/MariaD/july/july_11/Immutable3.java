0dd37493a88a044a11c0c1521fa8aca2cf9053ca
bws <mdunca92>
2022-07-12 14:04:34 +0300

    commit updated

--- src/main/java/MariaD/july/july_11/Immutable3.java
+++ src/main/java/MariaD/july/july_11/Immutable3.java
@@ -15,36 +15,42 @@ class NotImmutable {
   public StringBuilder getBuilder() {
     return builder;
   }
+}
+// create a defensive copy (a copy of mutuable object).
 
-  // create a defensive copy (a copy of mutuable object).
+class Mutable {
+  private StringBuilder builder;
 
-  /*public  Mutable(StringBuilder b) {
-      builder = new StringBuilder(b);
+  public Mutable(StringBuilder b) {
+    builder = new StringBuilder(b);
   }
+
   public StringBuilder getBuilder2() {
-      return new StringBuilder(builder);
+    return new StringBuilder(builder);
   }
+
   public String getValue() {
-      return builder.toString();
+    return builder.toString();
   }
-  */
 }
 
 public class Immutable3 {
   public static void main(String... args) {
 
-    StringBuilder sb = new StringBuilder("Initial");
-    NotImmutable problem = new NotImmutable(sb);
+    StringBuilder sb = new StringBuilder("Initialb vhghokkp");
+    // NotImmutable problem = new NotImmutable(sb);
+    Mutable problem = new Mutable(sb);
     sb.append(" added");
-    StringBuilder gotbuilder = problem.getBuilder();
+    StringBuilder gotbuilder = problem.getBuilder2();
     gotbuilder.append(" more");
-    System.out.println(
-        problem.getBuilder()); // Initial added more we are passing the same stringbuilder all over
-
-    //   the caller has a reference since it was passed to the constructor.
-    //   when we use stringbuilder.append, we ammend/append the reference, it is not immutable
-    // anymore
-    //   create a defensive copy (a copy of mutuable object).
-
+    // System.out.println(
+    // problem.getBuilder());////  Initial added more ------ we are passing the same
+    // stringbuilder all over
+    System.out.println(problem.getBuilder2()); // Initial
+    /*  the caller has a reference since it was passed to the constructor.
+     when we use stringbuilder.append, we ammend/append the reference, it is not immutable
+      anymore
+     create a defensive copy (a copy of mutuable object).
+    */
   }
 }
