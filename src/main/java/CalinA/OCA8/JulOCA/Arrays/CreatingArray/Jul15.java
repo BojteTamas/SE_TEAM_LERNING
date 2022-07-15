b229eeb25adf5eb20f006514b4916c31a1c37630
CalinAndreca <101342607+CalinAndreca@users.noreply.github.com>
2022-07-15 13:57:01 +0300

    OCAJul15 (#386)
    Co-authored-by: bws <bws@bwss-MacBook-Pro.local>

---
+++ src/main/java/CalinA/OCA8/JulOCA/Arrays/CreatingArray/Jul15.java
@@ -0,0 +1,19 @@
+package CalinA.OCA8.JulOCA.Arrays.CreatingArray;
+
+public class Jul15 {
+  public static void main(String[] args) {
+    int[] arr;
+
+    arr = new int[10];
+    arr[0] = 15;
+    arr[1] = 20;
+    arr[2] = 25;
+    arr[3] = 30;
+    arr[4] = 35;
+
+    for (int x = 0; x < arr.length; x++) System.out.println("Index " + x + " = " + arr[x]);
+  }
+}
+//// Read from OCA
+////        Creating an Array of Primitives
+// Understanding Java Arrays
