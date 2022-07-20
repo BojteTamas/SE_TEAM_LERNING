524b2d0e716a37ddc953fff73d64b7ea8050d82f
CalinAndreca <101342607+CalinAndreca@users.noreply.github.com>
2022-07-20 17:03:36 +0300

    OCAJul20 (#399)
    Co-authored-by: bws <bws@bwss-MacBook-Pro.local>

---
+++ src/main/java/CalinA/OCA8/JulOCA/Arrays/CreatingArray/Jul20Sorting1.java
@@ -0,0 +1,13 @@
+package CalinA.OCA8.JulOCA.Arrays.CreatingArray;
+
+import java.util.Arrays;
+
+public class Jul20Sorting1 {
+  public static void main(String[] args) {
+    int[] arr = {10, 55, 33, 67, 98, 81, 74, 92};
+    Arrays.sort(arr);
+
+    System.out.printf("From small to big: ", Arrays.toString(arr));
+  }
+}
+// nu returneaza nimic, mai trebuie sa verific dc n returneaza nimic
