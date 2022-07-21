317895c4f76cdfd683a9ad23eec205c16ca39dfd
Maria <98758996+mdunca92@users.noreply.github.com>
2022-07-21 12:37:50 +0300

    mariad daily ci constructor rules, inherited class members and overri… (#400)
    * mariad daily ci constructor rules, inherited class members and overriding
    
    * updated file
    
    Co-authored-by: bws <mdunca92>

---
+++ src/main/java/MariaD/Chapter_5/Compiller_Enhacement/Wolf.java
@@ -0,0 +1,22 @@
+package MariaD.Chapter_5.Compiller_Enhacement;
+
+import java.util.*;
+
+// creating a new class
+class Canine {
+  public double getAverageWeight() {
+    return 50;
+  }
+}
+
+public class Wolf extends Canine {
+  public double getAverageWeight() {
+    // overriding the method getAverageWeight() using the super keyword
+    return super.getAverageWeight() + 20;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new Canine().getAverageWeight()); // 50
+    System.out.println(new Wolf().getAverageWeight()); // 70
+  }
+}
