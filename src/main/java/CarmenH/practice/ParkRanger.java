d66eca27be1f8b730c2f1187d2bd03f20c485910
Carmen <chobinca@github.com>
2022-07-22 16:43:42 +0300

    fix

--- src/main/java/CarmenH/practice/ParkRanger.java
+++ src/main/java/CarmenH/practice/ParkRanger.java
@@ -1,16 +1,17 @@
 package CarmenH.practice;
 
-    public class ParkRanger {
-        int birds = 10;
-        public static void main(String[] data) {
-            int trees = 5;
-            System.out.print(trees+birds);
-        }
-    }
-//non static birds cannot be used in static main
+public class ParkRanger {
+  int birds = 10;
+
+  public static void main(String[] data) {
+    int trees = 5;
+    System.out.print(trees + birds);
+  }
+}
+// non static birds cannot be used in static main
 /**
- * The main() method is static and does not have access to any class instance variables.
- * The birds variable is not static and requires a class instance variable to access.
- * Therefore, the code does not compile when the static method attempts to access a non-static
- * variable without an instance of the class.
+ * The main() method is static and does not have access to any class instance variables. The birds
+ * variable is not static and requires a class instance variable to access. Therefore, the code does
+ * not compile when the static method attempts to access a non-static variable without an instance
+ * of the class.
  */
