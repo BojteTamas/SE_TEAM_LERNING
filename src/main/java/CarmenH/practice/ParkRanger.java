6f72cdd101d6f69515b637d0bb035c457f2b8e5a
Carmen <chobinca@github.com>
2022-07-22 16:40:59 +0300

    22jul22 - practice questions

---
+++ src/main/java/CarmenH/practice/ParkRanger.java
@@ -0,0 +1,16 @@
+package CarmenH.practice;
+
+    public class ParkRanger {
+        int birds = 10;
+        public static void main(String[] data) {
+            int trees = 5;
+            System.out.print(trees+birds);
+        }
+    }
+//non static birds cannot be used in static main
+/**
+ * The main() method is static and does not have access to any class instance variables.
+ * The birds variable is not static and requires a class instance variable to access.
+ * Therefore, the code does not compile when the static method attempts to access a non-static
+ * variable without an instance of the class.
+ */
---
+++ src/main/java/CarmenH/practice/Practice.java
@@ -0,0 +1,15 @@
+package CarmenH.practice;
+
+public class Practice {
+    static String weight = " A lot"; //class variable
+    /*default*/ double ageMonths = 5, ageDays = 2; //instance variable
+    private static boolean success = true; //class variable
+    public void main( String[] args) {
+        final String retries="1";
+        //P1
+    }
+}
+//number of variables Accessible at P1 - because main is not static
+//class - 2
+//Instance - 2
+//local - 1
