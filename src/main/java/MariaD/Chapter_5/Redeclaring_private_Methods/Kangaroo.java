0a0359fb11f23e87072cab8a7712a58c031ad6cb
bws <mdunca92>
2022-07-22 21:33:23 +0300

    updated file

--- src/main/java/MariaD/Chapter_5/Redeclaring_private_Methods/Kangaroo.java
+++ src/main/java/MariaD/Chapter_5/Redeclaring_private_Methods/Kangaroo.java
@@ -1,23 +1,29 @@
 package MariaD.Chapter_5.Redeclaring_private_Methods;
+
 import java.util.*;
-class Marsupial{
-     public static boolean isBiped() {
-        return false;
-    }
-    public void getDescription(){
-        System.out.println("marsupial walks on 2 legs: " + isBiped());
-    }
+
+class Marsupial {
+  public static boolean isBiped() {
+    return false;
+  }
+
+  public void getDescription() {
+    System.out.println("marsupial walks on 2 legs: " + isBiped());
+  }
 }
+
 public class Kangaroo extends Marsupial {
-    public static boolean isBiped() {
-        return true;
-    }
-    public void getKangarooDescription(){
-        System.out.println("kangaroo hops on 2 legs " +isBiped());
-    }
-    public static void main(String[] args){
-        Kangaroo jim = new Kangaroo();
-        jim.getDescription();//marsupial walks on 2 legs: false
-        jim.getKangarooDescription();//kangaroo hops on 2 legs true
-    }
+  public static boolean isBiped() {
+    return true;
+  }
+
+  public void getKangarooDescription() {
+    System.out.println("kangaroo hops on 2 legs " + isBiped());
+  }
+
+  public static void main(String[] args) {
+    Kangaroo jim = new Kangaroo();
+    jim.getDescription(); // marsupial walks on 2 legs: false
+    jim.getKangarooDescription(); // kangaroo hops on 2 legs true
+  }
 }
