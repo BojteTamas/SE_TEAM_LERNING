62a0355def0740a156412661a8c74d571907c192
bws <bws@bwss-MacBook-Pro.local>
2022-07-25 17:07:10 +0300

    OCAJul25

---
+++ src/main/java/CalinA/OCA8/JulOCA/Arrays/CreatingArray/Jul25Multidimensionalarrays.java
@@ -0,0 +1,11 @@
+package CalinA.OCA8.JulOCA.Arrays.CreatingArray;
+
+public class Jul25Multidimensionalarrays {
+  public static void main(String[] args) {
+    int[][] arr = {{5, 10}, {15, 20}};
+    for (int x = 0; x < 2; x++)
+      for (int y = 0; y < 2; y++) System.out.println("Array " + x + " = " + y + " = " + arr[x][y]);
+  }
+}
+// Read from OCA
+// Multidimensional Array
